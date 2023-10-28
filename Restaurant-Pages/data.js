eff4323a0a8089bc98ab0fd8aa21b0ad9ead6f2e
waritctd <waritsara5487@gmail.com>
2023-10-29 01:13:16 +0700

    renamed:    counter.js -> Explore-Page/counter.js 	renamed:    resource/favorite.txt -> Explore-Page/resource/favorite.txt 	renamed:    resource/favourite.svg -> Explore-Page/resource/favourite.svg 	renamed:    resource/videos/video1.MP4 -> Explore-Page/resource/video1.MP4 	renamed:    resource/videos/video2.MP4 -> Explore-Page/resource/video2.MP4 	renamed:    resource/videos/video3.MP4 -> Explore-Page/resource/video3.MP4 	renamed:    resource/videos/video4.MP4 -> Explore-Page/resource/video4.MP4 	renamed:    style.css -> Explore-Page/style.css 	renamed:    globals.css -> Home-Page/globals.css 	renamed:    img/cap.svg -> Home-Page/img/cap.svg 	renamed:    img/cellular-connection.svg -> Home-Page/img/cellular-connection.svg 	renamed:    img/filter.svg -> Home-Page/img/filter.svg 	renamed:    img/frame-24.svg -> Home-Page/img/frame-24.svg 	new file:   Home-Page/img/frame-34.png 	new file:   Home-Page/img/frame-36.png 	new file:   Home-Page/img/frame-37.png 	new file:   Home-Page/img/frame-9.png 	renamed:    img/icon-edit.svg -> Home-Page/img/icon-edit.svg 	renamed:    img/icon-home-alt.svg -> Home-Page/img/icon-home-alt.svg 	renamed:    img/mask-group.png -> Home-Page/img/mask-group.png 	renamed:    img/photo-video-video.svg -> Home-Page/img/photo-video-video.svg 	renamed:    img/shopping-cart-icon.png -> Home-Page/img/shopping-cart-icon.png 	renamed:    img/star-6.png -> Home-Page/img/star-6.png 	renamed:    img/wifi.svg -> Home-Page/img/wifi.svg 	renamed:    style_home.css -> Home-Page/style_home.css 	renamed:    script_quiz.js -> Quiz-Page/script_quiz.js 	renamed:    style_quiz.css -> Quiz-Page/style_quiz.css 	renamed:    img/Calzone.jpeg -> Restaurant-Pages/Restaurant1/img/Calzone.jpeg 	renamed:    img/Tony.jpeg -> Restaurant-Pages/Restaurant1/img/Header.jpeg 	renamed:    img/Margherita.jpeg -> Restaurant-Pages/Restaurant1/img/Margherita.jpeg 	new file:   Restaurant-Pages/Restaurant1/img/shopping-cart-icon.png 	new file:   Restaurant-Pages/Restaurant1/menu_list.js 	new file:   Restaurant-Pages/Restaurant1/restaurant.html 	renamed:    style_restaurant.css -> Restaurant-Pages/Restaurant1/style_restaurant.css 	new file:   Restaurant-Pages/data.js 	new file:   Shopping-Cart Page/cart.js 	renamed:    finish.html -> Shopping-Cart Page/finish.html 	renamed:    img/Motorbike-removebg.png -> Shopping-Cart Page/img/Motorbike-removebg.png 	renamed:    img/Motorbike.jpg -> Shopping-Cart Page/img/Motorbike.jpg 	renamed:    style_finish.css -> Shopping-Cart Page/style_finish.css 	renamed:    style_summary.css -> Shopping-Cart Page/style_summary.css 	modified:   explore.html 	deleted:    img/ellipse-7.svg 	deleted:    img/frame 34.png 	deleted:    img/frame 9.png 	deleted:    img/frame-36.png 	deleted:    img/frame-37.png 	deleted:    img/star-1.svg 	deleted:    img/star-5-4.svg 	deleted:    img/star-5.png 	deleted:    img/star-6-4.svg 	deleted:    img/vuesax-linear-message-question.svg 	modified:   index.html 	modified:   quiz.html 	deleted:    restaurant.html 	deleted:    script.js 	modified:   summary.html

---
+++ Restaurant-Pages/data.js
@@ -0,0 +1,26 @@
+let menuItemsData = [
+    {
+        id: "menu1",
+        name: "Margherita Pizza",
+        price: "109",
+        img: "img/Margherita.jpeg"
+    },
+    {
+        id: "menu2",
+        name: "Pepperoni Pizza",
+        price: "119",
+        img: "img/Margherita.jpeg"
+    },
+    {
+        id: "menu3",
+        name: "Vegetarian Pizza",
+        price: "129",
+        img: "img/Margherita.jpeg"
+    },
+    {
+        id: "menu4",
+        name: "Hawaiian Pizza",
+        price: "139",
+        img: "img/Margherita.jpeg"
+    }
+];
